beb7c63c9d5efdf0d634d41111b80eb7116915d7
Jao-Couto <73503848+Jao-Couto@users.noreply.github.com>
2021-07-26 16:02:27 -0300

    update ip

--- src/database/database.providers.ts
+++ src/database/database.providers.ts
@@ -5,7 +5,7 @@ export const databaseProviders = [
     provide: 'DATABASE_CONNECTION',
     useFactory: async () => await createConnection({
       type: 'mysql',
-      host: '192.168.1.240',
+      host: 'localhost',
       port: 3306,
       username: 'root',
       password: '',
