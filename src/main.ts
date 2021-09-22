9d720b1d45d50a7bbf06e049c94ee71d4bb210be
Jao-Couto <73503848+Jao-Couto@users.noreply.github.com>
2021-09-22 11:18:18 -0300

    heroku master

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,7 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  var port = process.env.port || 3000;
+  var port = process.env.port || 3306;
   await app.listen(port);
   
 }
