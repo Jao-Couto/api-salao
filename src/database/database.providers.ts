c015072f21b15f7cf3b0eca095c13158e67228e4
Jao-Couto <73503848+Jao-Couto@users.noreply.github.com>
2021-09-21 21:22:51 -0300

    heroku master

--- src/database/database.providers.ts
+++ src/database/database.providers.ts
@@ -5,11 +5,11 @@ export const databaseProviders = [
     provide: 'DATABASE_CONNECTION',
     useFactory: async () => await createConnection({
       type: 'mysql',
-      host: 'localhost',
+      host: 'us-cdbr-east-04.cleardb.com',
       port: 3306,
-      username: 'root',
-      password: '',
-      database: 'appsalao',
+      username: 'b01b138a733528',
+      password: '9bb8ffa5',
+      database: 'heroku_9acee264d36c327',
       entities: [
           __dirname + '/../**/*.entity{.ts,.js}',
       ],
